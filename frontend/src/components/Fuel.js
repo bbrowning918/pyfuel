a6257b70e5007643983f9c5f8eac5cf01ef25db1
Ben <benbrowning918@gmail.com>
2019-03-12 11:09:11 -0600

    Update Fuel.js
    restructured how chart gets formatted
    colour generation is now more distinct

--- frontend/src/components/Fuel.js
+++ frontend/src/components/Fuel.js
@@ -1,55 +1,52 @@
 import React, { Component } from "react"
 import {Line} from 'react-chartjs-2'
-import { groupBy, find } from 'lodash'
+import { groupBy, find, forEach } from 'lodash'
 import moment, { utc }  from 'moment'
 import { randomColor } from 'randomcolor'
 
 class Fuel extends Component {
   constructor(props) {
     super()
+
+    //should try and pass/inject these from the bootstrap theme
+    this.hues = ['#3498DB', '#E74C3C', '#fd7e14', '#F39C12', '#18BC9C', '#e83e8c']
   }
 
-  //formats chartable points from a data set
-  //x axis is datetime
-  //y axis is liters per 100 km
-  chartFuelEconomy(date,liters,distance) {
-    return {
-      x: utc(date, moment.ISO_8601).format('MM/DD/YYYY HH:mm'),
-      y: Number.parseFloat(liters / (distance / 100)).toFixed(2)
-    }
-  }
-
-  render() {
+  //takes a lodash.groupBy object and formats our data for our chart
+  formatDataSet(groups) {
     const dataset = []
 
-    //produces an object composed of arrays keyed on 'vehicle' (ie a url)
-    //that contains the grouped elements (fuel objects)
-    const fuel_groups = groupBy(this.props.fuel, 'vehicle')
+    //how to pass proper this into the lodash.forEach local function?
+    const hues = this.hues
+    const vehicles = this.props.vehicles 
 
-    //fuel_group[0] is the vehicle's url identifier
-    //fuel_group[1] holds the array of fuel data
-    for (const fuel_group of Object.entries(fuel_groups)){
+    //extra counter used for colour generation
+    let i = 0
+
+    forEach (groups, function(fuel_array, vehicle_key,) {
+
+      //generate a (random) colour sequentially by looping on our hues array
+      //guarentees distinct (and hopefully appealing) colours up to hues.length
+      const colour = randomColor({luminosity: 'bright', hue: hues[i % hues.length]})
+      i++
+
+      //pull vehicle data based on matching vehicle key (url)
+      const vehicle = find(vehicles, function(v) { return v.url === vehicle_key})
       
+      //loop through the fuel data and map it to chartable x,y coordinates
       const data_points = []
-      
-      //pull vehicle data based on matching vehicle url
-      const vehicle = find(this.props.vehicles, function(v) { return v.url === fuel_group[0]})
-
-      //loop through the fuel data and map it to chartable objects (x,y)
-      for (const fuel_entry of Object.values(fuel_group[1])){
+      for (const fuel_entry of fuel_array.values()){
         data_points.push(
-          this.chartFuelEconomy(
-            fuel_entry['date'],
-            fuel_entry['liters'],
-            fuel_entry['distance']
-          )
+          {
+            //x axis is a formatted datetime stamp
+            x: utc(fuel_entry['date'], moment.ISO_8601).format('DD/MM/YYYY HH:mm'),
+            //y axis is L/100km, rounded to 2 digits
+            y: Number.parseFloat(fuel_entry['liters'] / (fuel_entry['distance'] / 100)).toFixed(2)
+          }
         )
       }
       
-      //generates a colour based on the url (bright and same hue as sample)
-      const colour = randomColor({luminosity: 'bright', hue: '#3498DB', seed: fuel_group[0]})
-
-      //construct and format our dataset for this vehicle and add to the set
+      //construct and format our data for this vehicle and add to the set
       dataset.push({
         label: `${vehicle.year} ${vehicle.manufacturer} ${vehicle.model}`,
         fill: false,
@@ -61,20 +58,28 @@ class Fuel extends Component {
         pointHitRadius: 5,
         data: data_points
       })
-    }
+    }) 
+
+    return dataset
+  }
+
+  render() {
+    //produces an object composed of arrays keyed on 'vehicle' (ie the vehicle's url)
+    //the array's values are the fuel objects
+    const data = groupBy(this.props.fuel, 'vehicle')
 
     return (
       <div>
       <h2>Fuel Economy</h2>
         <Line 
-          data={{ datasets: dataset }}
+          data={{ datasets: this.formatDataSet(data) }}
           height={ 110 }
           options={{
             scales: {
               xAxes: [{
                 type: 'time',
                 time: {
-                  parser: 'MM/DD/YYYY HH:mm',
+                  parser: 'DD/MM/YYYY HH:mm',
                   tooltipFormat: 'll HH:mm'
                 },
                 scaleLabel: {
